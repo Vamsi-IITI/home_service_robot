f88a7204896cd5869fd2fd9534e4c32266a3e6f8
Vamsi <rvb20ubuntu@gmail.com>
2023-04-03 22:55:02 +0530

    Update test_navigation.sh
    Solving the issue :
    # Option “-e” is deprecated and might be removed in a later version of gnome-terminal.
    # Use “-- ” to terminate the options and put the command line to execute after it.

--- src/scripts/test_navigation.sh
+++ src/scripts/test_navigation.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # launch turtlebot_world.launch to deploy turtlebot environment
-gnome-terminal -e 'bash -c "source ~/home_service_robot/devel/setup.bash"';
-gnome-terminal -e 'bash -c "roslaunch autonomous_turtlebot3 turtlebot3_world.launch "' & 
+gnome-terminal -- bash -c "source ~/home_service_robot/devel/setup.bash";
+gnome-terminal -- bash -c "roslaunch autonomous_turtlebot3 turtlebot3_world.launch " & 
 
 sleep 5
 
 # launch turtlebot3_slam.launch to perform slam_gmapping
-gnome-terminal -e 'bash -c "source ~/home_service_robot/devel/setup.bash"';
-gnome-terminal -e 'bash -c "roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:=$HOME/home_service_bot/src/autonomous_turtlebot3/map/home1.yaml "'
+gnome-terminal -- bash -c "source ~/home_service_robot/devel/setup.bash";
+gnome-terminal -- bash -c "roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:=$HOME/home_service_bot/src/autonomous_turtlebot3/map/home1.yaml "
